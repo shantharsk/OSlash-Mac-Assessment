dba7fa7e0209264115be0968e925de08756f80a5
shantharsk <shantharsk@gmail.com>
2022-09-05 21:54:47 +0530

    initial commit

---
+++ OSlash Preferences/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OSlash Preferences
+//
+//  Created by Shantha on 9/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
